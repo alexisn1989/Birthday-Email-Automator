63aab8a1c127ae186a754f534692eceb71d3e2a4
Alexis Nieuwenhuys <alexisnieuwenhuys89@gmail.com>
2026-03-22 00:10:39 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MY_EMAIL = os.environ.get("MY_EMAIL")
+PASSWORD = os.environ.get("MY_PASSWORD")
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_dict[today_tuple]['name'])
+
+    with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_dict[today_tuple]['email'],
+            msg=f"Subject: Happy Birthday!\n\n{contents}"
+        )
+
+
